f41eb8b5448614dfefce6270d26864b429b4996f
Ethan Slattery <crustyauklet@gmail.com>
2020-11-24 18:42:05 -0800

    add initial code

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+// Copyright 2020 Embedded Artistry LLC
+// SPDX-License-Identifier: MIT
+
+#include <platform.hpp>
+
+volatile bool abort_program_ = false;
+
+int main()
+{
+	printf("Blinky application booted!\n");
+
+	auto& platform = VirtualPlatform::inst();
+
+	printf("Starting blink\n");
+	platform.startBlink();
+
+	while(!abort_program_)
+	{
+		// spin
+		// embvm::this_thread::sleep_for(std::chrono::milliseconds(250));
+	}
+
+	return 0;
+}
